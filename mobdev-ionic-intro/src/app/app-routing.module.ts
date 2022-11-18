11b99b6f0370c1be0331a025ff92377659700447
fatihelgormus <2022124@student.cct.ie>
2022-11-18 22:41:57 +0000

    we have some pages now

--- mobdev-ionic-intro/src/app/app-routing.module.ts
+++ mobdev-ionic-intro/src/app/app-routing.module.ts
@@ -11,6 +11,34 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'episodes',
+    loadChildren: () => import('./pages/episodes/episodes.module').then( m => m.EpisodesPageModule)
+  },
+  {
+    path: 'episodes-details',
+    loadChildren: () => import('./pages/episodes-details/episodes-details.module').then( m => m.EpisodesDetailsPageModule)
+  },
+  {
+    path: 'quotes',
+    loadChildren: () => import('./pages/quotes/quotes.module').then( m => m.QuotesPageModule)
+  },
+  {
+    path: 'death-count',
+    loadChildren: () => import('./pages/death-count/death-count.module').then( m => m.DeathCountPageModule)
+  },
+  {
+    path: 'tab',
+    loadChildren: () => import('./pages/tab/tab.module').then( m => m.TabPageModule)
+  },
+  {
+    path: 'characters',
+    loadChildren: () => import('./pages/characters/characters.module').then( m => m.CharactersPageModule)
+  },
+  {
+    path: 'characters-details',
+    loadChildren: () => import('./pages/characters-details/characters-details.module').then( m => m.CharactersDetailsPageModule)
+  },
 ];
 
 @NgModule({
