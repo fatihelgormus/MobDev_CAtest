bf667b07569f98007f9d06a68d025d1c68a95890
fatihelgormus <2022124@student.cct.ie>
2022-11-21 12:18:44 +0000

    some changes

--- mobdev-ionic-intro/src/test.ts
+++ mobdev-ionic-intro/src/test.ts
@@ -1,14 +1,15 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
-import 'zone.js/testing';
+import 'zone.js/dist/zone-testing';
 import { getTestBed } from '@angular/core/testing';
 import {
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting
 } from '@angular/platform-browser-dynamic/testing';
 
-// First, initialize the Angular testing environment.
-getTestBed().initTestEnvironment(
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting(),
-);
+declare const require: {
+  context(path: string, deep?: boolean, filter?: RegExp): {
+    <T>(id: string): T;
+    keys(): string[];
+  };
+};
